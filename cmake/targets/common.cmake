53f16cc51aa83a035aa5e2678a81429c37448a93
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-04-04 14:18:39 +0000

    linux build

--- cmake/targets/common.cmake
+++ cmake/targets/common.cmake
@@ -6,15 +6,15 @@ add_library(parent SHARED
         "${CMAKE_SOURCE_DIR}/lib/interprocess.cpp"
         )
 set_target_properties(parent PROPERTIES CXX_STANDARD 17)
-set_property(TARGET parent APPEND_STRING PROPERTY LINK_FLAGS " -static")
-set_property(TARGET parent APPEND_STRING PROPERTY LINK_FLAGS " -static-libgcc")
-set_property(TARGET parent APPEND_STRING PROPERTY LINK_FLAGS " -static-libstdc++")
 
 add_executable(sunshine ${SUNSHINE_TARGET_FILES})
 
 # platform specific target definitions
 if(WIN32)
     include(${CMAKE_MODULE_PATH}/targets/windows.cmake)
+    set_property(TARGET parent APPEND_STRING PROPERTY LINK_FLAGS " -static")
+    set_property(TARGET parent APPEND_STRING PROPERTY LINK_FLAGS " -static-libgcc")
+    set_property(TARGET parent APPEND_STRING PROPERTY LINK_FLAGS " -static-libstdc++")
 elseif(UNIX)
     include(${CMAKE_MODULE_PATH}/targets/unix.cmake)
 
