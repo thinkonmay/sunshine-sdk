8d56349b2466c3e6a003ab93614f506c946e7e07
pigeatgarlic <huyhoangdo0205@gmail.com>
2026-04-05 21:45:53 +0700

    remove cmake

--- cmake/macros/common.cmake
+++
@@ -1,7 +0,0 @@
-# common macros
-# this file will also load platform specific macros
-
-# platform specific macros
-if(WIN32)
-    include(${CMAKE_MODULE_PATH}/macros/windows.cmake)
-endif()
--- cmake/prep/special_package_configuration.cmake
+++
@@ -1,9 +0,0 @@
-
-# return if configure only is set
-if(${SUNSHINE_CONFIGURE_ONLY})
-    # message
-    message(STATUS "SUNSHINE_CONFIGURE_ONLY: ON, exiting...")
-    set(END_BUILD ON)
-else()
-    set(END_BUILD OFF)
-endif()
